bcc202ab773120aa5b5cd5e89041e7229dcddad7
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-17 12:34:26 +0200

    add socket io production configutation

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const server = require("http").Server(app);
 
 //for localhost  => change origins if I want to put Social Network online
-const io = require("socket.io")(server, {
-    origins: "localhost:8080",
-});
+// const io = require("socket.io")(server, {
+//     origins: "localhost:8080",
+// });
 // socket.io live configuration
-// const io = require("socket.io").listen(server);
+const io = require("socket.io").listen(server);
 
 const compression = require("compression");
 const bodyParser = require("body-parser");
